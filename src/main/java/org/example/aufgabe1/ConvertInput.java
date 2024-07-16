99b26e542880c75a7f4e494212366f821ad6743c
Vera Peuntinger <vera.peuntinger@innoq.com>
2024-07-16 13:27:19 +0200

    small refactoring

--- src/main/java/org/example/aufgabe1/ConvertInput.java
+++ src/main/java/org/example/aufgabe1/ConvertInput.java
@@ -6,30 +6,29 @@ import java.util.Map;
 
 public class ConvertInput {
 
-    private final Map<Integer, String> translations;
+    private final Map<String, String> translations;
 
     public ConvertInput() {
         this.translations = new HashMap<>();
-        translations.put(0, "null");
-        translations.put(1, "eins");
-        translations.put(2, "zwei");
-        translations.put(3, "drei");
-        translations.put(4, "vier");
-        translations.put(5, "fünf");
-        translations.put(6, "sechs");
-        translations.put(7, "sieben");
-        translations.put(8, "acht");
-        translations.put(9, "neun");
-        translations.put(10, "zehn");
+        translations.put("0", "null");
+        translations.put("1", "eins");
+        translations.put("2", "zwei");
+        translations.put("3", "drei");
+        translations.put("4", "vier");
+        translations.put("5", "fünf");
+        translations.put("6", "sechs");
+        translations.put("7", "sieben");
+        translations.put("8", "acht");
+        translations.put("9", "neun");
+        translations.put("10", "zehn");
     }
 
     public String convert(List<String> input) {
         StringBuilder result = new StringBuilder();
         for (String s : input) {
-            try {
-                int i = Integer.parseInt(s);
-                result.append(translations.get(i));
-            } catch (NumberFormatException e) {
+            if(translations.containsKey(s)) {
+                result.append(translations.get(s));
+            } else {
                 result.append(s);
             }
         }
